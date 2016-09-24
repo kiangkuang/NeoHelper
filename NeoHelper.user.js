1b56e4bd3dfd68c79da5ec29e1fb5244551dc1b0
Kiang Kuang <kiangkuang@gmail.com>
2016-09-25 03:58:23 +0800

    handle not logged in and not enough np

--- NeoHelper.user.js
+++ NeoHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NeoHelper
 // @namespace    https://github.com/kiangkuang
-// @version      0.1
+// @version      0.2
 // @description  Buys stocks and visit the shrine for you everyday
 // @author       Kiang Kuang
 // @include      *
@@ -12,67 +12,104 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js
 // ==/UserScript==
 
-//GM_deleteValue('boughtStocks');
-//GM_deleteValue('buyingStocks');
-//GM_deleteValue('clickedShrine');
-//GM_deleteValue('clickingShrine');
-
-nst = moment().utcOffset(-7);
-boughtStocks = GM_getValue('boughtStocks', 0);
-buyingStocks = GM_getValue('buyingStocks', false);
-clickedShrine = GM_getValue('clickedShrine', 0);
-clickingShrine = GM_getValue('clickingShrine', false);
-
-function goToStocks() {
-    $.ajax("http://www.neopets.com/stockmarket.phtml?type=list&full=true")
-        .done(function(data) {
-            result = $.parseHTML(data);
-
-            for (price = 15; price < 17; price++) {
-                var company = $($(result).find('.content td:nth-child(6):contains(' + price + ')').siblings()[1]).find('a').text();
-                if (company) {
-                    GM_setValue('buyingStocks', true);
-                    window.location.href = "http://www.neopets.com/stockmarket.phtml?type=buy&ticker=" + company;
-                    break;
-                }
-            }
-        });
-}
-
-function buyStocks() {
-    $('.content input[name="amount_shares"]').val('1000');
-    $('.content form').submit();
-}
-
-function goToShrine() {
-    window.location.href = 'http://www.neopets.com/desert/shrine.phtml';
-}
-
-function clickShrine() {
-    $('.content form:nth-child(3)').submit();
-}
-
 (function() {
+    "use strict";
 
-if (moment(boughtStocks).isBefore(nst, 'day') && !buyingStocks) {
-    goToStocks();
-} else if (document.URL.indexOf('/stockmarket.phtml?type=buy&ticker=') > -1 && buyingStocks) {
-    GM_setValue('buyingStocks', false);
-    GM_setValue('boughtStocks', moment().utcOffset(-7).format());
-    buyStocks();
-} else if (moment(clickedShrine).isBefore(nst, 'day') && !clickingShrine) {
-    GM_setValue('clickingShrine', true);
-    goToShrine();
-} else if (window.location.pathname == '/desert/shrine.phtml' && clickingShrine) {
-    GM_setValue('clickingShrine', false);
-    GM_setValue('clickedShrine', moment().utcOffset(-7).format());
-    clickShrine();
-}
+    var config = {
+        buyStocks: true,
+        minPrice: 15,
+        maxPrice: 17,
+        stocksAmount: 1000,
+        visitShrine: true,
+    };
 
+    //reset();
+
+    var boughtStocks = GM_getValue('boughtStocks', 0);
+    var buyingStocks = GM_getValue('buyingStocks', false);
+    var clickedShrine = GM_getValue('clickedShrine', 0);
+    var clickingShrine = GM_getValue('clickingShrine', false);
+
+    var nst = moment().utcOffset(-7);
+
+    if (config.buyStocks) {
+        if (moment(boughtStocks).isBefore(nst, 'day') && !buyingStocks) {
+            goToStocks();
+            return;
+        }
+        if (document.URL.indexOf('/stockmarket.phtml?type=buy&ticker=') > -1 && buyingStocks) {
+            GM_setValue('buyingStocks', false);
+            GM_setValue('boughtStocks', moment().utcOffset(-7).format());
+            buyStocks();
+            return;
+        }
+    }
+
+    if (config.visitShrine) {
+        if (moment(clickedShrine).isBefore(nst, 'day') && !clickingShrine) {
+            GM_setValue('clickingShrine', true);
+            goToShrine();
+            return;
+        }
+        if (window.location.pathname == '/desert/shrine.phtml' && clickingShrine) {
+            GM_setValue('clickingShrine', false);
+            GM_setValue('clickedShrine', moment().utcOffset(-7).format());
+            clickShrine();
+            return;
+        }
+    }
+
+    function goToStocks() {
+        $.ajax("http://www.neopets.com/stockmarket.phtml?type=list&full=true")
+            .done(function(data) {
+                var result = $.parseHTML(data);
+
+                if ($(result).find('.welcomeContent').length === 0) {
+                    for (var price = config.minPrice; price <= config.maxPrice; price++) {
+                        var company = $($(result).find('.content td:nth-child(6):contains(' + price + ')').siblings()[1]).find('a').text();
+                        if (company) {
+                            GM_setValue('buyingStocks', true);
+                            GM_setValue('stockPrice', price);
+                            window.location.href = "http://www.neopets.com/stockmarket.phtml?type=buy&ticker=" + company;
+                            break;
+                        }
+                    }
+                } else {
+                    console.error("NeoHelper: Not logged in!");
+                }
+            });
+    }
+
+    function buyStocks() {
+        var amt = Math.min(config.stocksAmount, Math.floor(parseInt($("#npanchor").text().replace(/,/g, '')) / GM_getValue('stockPrice')));
+        $('.content input[name="amount_shares"]').val(amt);
+
+        if (amt < config.stocksAmount) {
+            alert("Not enough NP for " + config.stocksAmount + " shares!");
+            return;
+        }
+
+        $('.content form').submit();
+    }
+
+    function goToShrine() {
+        window.location.href = 'http://www.neopets.com/desert/shrine.phtml';
+    }
+
+    function clickShrine() {
+        $('.content form:nth-child(3)').submit();
+    }
+
+    function reset() {
+        GM_deleteValue('boughtStocks');
+        GM_deleteValue('buyingStocks');
+        GM_deleteValue('clickedShrine');
+        GM_deleteValue('clickingShrine');
+        GM_deleteValue('stockPrice');
+    }
 })();
 
 /* TODO
-
 if (window.location.pathname == '/medieval/potatocounter.phtml') {
   //alert($('#content > table > tbody > tr > td.content > table > tbody > tr > td > img').length);
   $('#content > table > tbody > tr > td.content > center:nth-child(7) > form > input[type="text"]:nth-child(2)').val($('#content > table > tbody > tr > td.content > table > tbody > tr > td > img').length);
@@ -85,5 +122,4 @@ if (window.location.pathname == '/donations.phtml') {
   console.log(window.location.pathname);
   window.location.href = '/donations.phtml';
 }
-
 */
