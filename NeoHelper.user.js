ecb3d7cba135643dcc409eaebbc8d7f51da5e830
Kiang Kuang <kiangkuang@gmail.com>
2016-11-26 17:01:21 +0800

    support dst

--- NeoHelper.user.js
+++ NeoHelper.user.js
@@ -1,16 +1,17 @@
 // ==UserScript==
 // @name         NeoHelper
 // @namespace    https://github.com/kiangkuang
-// @version      0.5.2
+// @version      0.5.3
 // @author       Kiang Kuang
-// @description  Helps you to buy stocks daily, visit Clotzan's Shrine daily, visit Trudy's Surprise daily, play Potato Counter, take items from Money Tree
+// @description  Helps you to buy stocks daily, visit Coltzan's Shrine daily, visit Trudy's Surprise daily, play Potato Counter, take items from Money Tree
 // @homepage     https://github.com/kiangkuang/NeoHelper
 // @supportURL   https://github.com/kiangkuang/NeoHelper/issues
 // @updateURL    https://github.com/kiangkuang/NeoHelper/raw/master/NeoHelper.user.js
 // @downloadURL  https://github.com/kiangkuang/NeoHelper/raw/master/NeoHelper.user.js
 // @include      *
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.0/moment.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.9/moment-timezone-with-data.min.js
 // @connect      neopets.com
 // @grant        GM_getValue
 // @grant        GM_setValue
@@ -19,7 +20,6 @@
 // @grant        GM_xmlhttpRequest
 // @noframes
 // ==/UserScript==
-
 (function() {
     "use strict";
 
@@ -35,53 +35,53 @@
         moneyTree: true,
     };
 
-    var nst = moment().utcOffset(-7);
+    var nst = moment.tz('America/Los_Angeles');
 
     if (config.buyStocks) {
-        if (moment(GM_getValue('boughtStocks', 0)).utcOffset(-7).isBefore(nst, 'day') && !GM_getValue('buyingStocks', false)) {
+        if (moment.tz(GM_getValue('boughtStocks', 0), 'America/Los_Angeles').isBefore(nst, 'day') && !GM_getValue('buyingStocks', false)) {
             GM_setValue('buyingStocks', true);
             goToStocks();
             return;
         }
         if (document.URL.indexOf('/stockmarket.phtml?type=buy&ticker=') > -1 && GM_getValue('buyingStocks', false)) {
             GM_setValue('buyingStocks', false);
-            GM_setValue('boughtStocks', moment().utcOffset(-7).format());
+            GM_setValue('boughtStocks', moment.tz('America/Los_Angeles').format());
             buyStocks();
             return;
         }
     }
 
     if (config.visitShrine) {
-        if (moment(GM_getValue('clickedShrine', 0)).utcOffset(-7).isBefore(nst, 'day') && !GM_getValue('clickingShrine', false)) {
+        if (moment.tz(GM_getValue('clickedShrine', 0), 'America/Los_Angeles').isBefore(nst, 'day') && !GM_getValue('clickingShrine', false)) {
             GM_setValue('clickingShrine', true);
             GM_openInTab('http://www.neopets.com/desert/shrine.phtml', true);
             return;
         }
         if (window.location.pathname == '/desert/shrine.phtml' && GM_getValue('clickingShrine', false)) {
             GM_setValue('clickingShrine', false);
-            GM_setValue('clickedShrine', moment().utcOffset(-7).format());
+            GM_setValue('clickedShrine', moment.tz('America/Los_Angeles').format());
             $('.content form:nth-child(3)').submit();
             return;
         }
     }
 
     if (config.fishing) {
-        if (moment(GM_getValue('clickedFishing', 0)).utcOffset(-7).isBefore(nst, 'day') && !GM_getValue('clickingFishing', false)) {
+        if (moment.tz(GM_getValue('clickedFishing', 0), 'America/Los_Angeles').isBefore(nst, 'day') && !GM_getValue('clickingFishing', false)) {
             GM_setValue('clickingFishing', true);
             GM_openInTab('http://www.neopets.com/water/fishing.phtml', true);
             return;
         }
         if (window.location.pathname == '/water/fishing.phtml' && GM_getValue('clickingFishing', false)) {
             GM_setValue('clickingFishing', false);
-            GM_setValue('clickedFishing', moment().utcOffset(-7).format());
+            GM_setValue('clickedFishing', moment.tz('America/Los_Angeles').format());
             $('.content form')[0].submit();
             return;
         }
     }
 
     if (config.trudysSurprise) {
-        if (moment(GM_getValue('trudysSurprise', 0)).utcOffset(-7).isBefore(nst, 'day')) {
-            GM_setValue('trudysSurprise', moment().utcOffset(-7).format());
+        if (moment.tz(GM_getValue('trudysSurprise', 0), 'America/Los_Angeles').isBefore(nst, 'day')) {
+            GM_setValue('trudysSurprise', moment.tz('America/Los_Angeles').format());
             GM_openInTab('http://www.neopets.com/trudys_surprise.phtml', true);
             return;
         }
@@ -126,7 +126,7 @@
                         }
                     }
                     GM_setValue('buyingStocks', false);
-                    GM_setValue('boughtStocks', moment().utcOffset(-7).format());
+                    GM_setValue('boughtStocks', moment.tz('America/Los_Angeles').format());
                     console.error("NeoHelper: No stocks within price range to buy!");
                 } else {
                     console.error("NeoHelper: Not logged in!");
